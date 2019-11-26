bf8e3d2ba68d65f39bbbb851ddcf577f019c8503
DewulfTjorven <tjorven.dewulf@student.howest.be>
2019-11-26 12:28:49 +0100

    removed unneccesary files and code to start the project clean

--- src/view/layout.php
+++ src/view/layout.php
@@ -2,22 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content ="initial-scale=1.0" width="device-width">
     <title>Todos - <?php echo $title; ?></title>
-    <?php /* NEW */ ?>
     <?php echo $css;?>
   </head>
   <body>
     <main>
-      <?php
-        if(!empty($_SESSION['error'])) {
-          echo '<div class="error box">' . $_SESSION['error'] . '</div>';
-        }
-        if(!empty($_SESSION['info'])) {
-          echo '<div class="info box">' . $_SESSION['info'] . '</div>';
-        }
-      ?>
-      <header><h1>Todos - <?php echo $title; ?></h1></header>
       <?php echo $content;?>
     </main>
     <?php echo $js; ?>
