8138036ebe9f9c43c24e99f7688cd7d9092e7da2
samdumoulin <sam.dumoulin@student.howest.be>
2019-11-27 11:28:44 +0100

    added users from database to dashboard #5

--- src/controller/FlancersController.php
+++ src/controller/FlancersController.php
@@ -5,66 +5,23 @@ require_once __DIR__ . '/../dao/FlancerDAO.php';
 
 class FlancersController extends Controller {
 
-  private $flancerDAO;
+  private $todoDAO;
 
   function __construct() {
     $this->flancerDAO = new FlancerDAO();
   }
 
-  public function index() {
-    /*
-    if (!empty($_POST['action'])) {
-      if ($_POST['action'] == 'insertFlancer') {
-        $this->handleInsertTodo();
-      }
-    }
-    */
+  public function dashboard() {
 
     $flancers = $this->flancerDAO->selectAll();
+
     $this->set('flancers', $flancers);
-    $this->set('title', 'Overview');
-/*
+
     if (strtolower($_SERVER['HTTP_ACCEPT']) == 'application/json') {
+
       header('Content-Type: application/json');
-      echo json_encode($todos);
-      exit();
-    }
-*/
-  }
-/*
-  private function handleInsertTodo() {
-    $data = array(
-      'created' => date('Y-m-d H:i:s'),
-      'modified' => date('Y-m-d H:i:s'),
-      'checked' => 0,
-      'text' => $_POST['text']
-    );
-    $insertTodoResult = $this->todoDAO->insert($data);
-    if (!$insertTodoResult) {
-      $errors = $this->todoDAO->validate($data);
-      $this->set('errors', $errors);
-      if (strtolower($_SERVER['HTTP_ACCEPT']) == 'application/json') {
-        header('Content-Type: application/json');
-        echo json_encode(array(
-          'result' => 'error',
-          'errors' => $errors
-        ));
-        exit();
-      }
-      $_SESSION['error'] = 'De todo kon niet toegevoegd worden!';
-    } else {
-      if (strtolower($_SERVER['HTTP_ACCEPT']) == 'application/json') {
-        header('Content-Type: application/json');
-        echo json_encode(array(
-          'result' => 'ok',
-          'todo' => $insertTodoResult
-        ));
-        exit();
-      }
-      $_SESSION['info'] = 'De todo is toegevoegd!';
-      header('Location: index.php');
+      echo json_encode($flancers);
       exit();
     }
   }
-*/
 }
--- src/view/flancers/dashboard.php
+++ src/view/flancers/dashboard.php
@@ -3,6 +3,15 @@
 </section>
 <section class="title--section">
   <h1>Available freelancers</h1>
+  <?php
+  foreach($flancers as $flancer){
+    ?>
+    <li class='flancer'>
+      <span class='flancer__firstname'><?php echo $flancer["firstname"];?></span>
+    </li>
+    <?php
+  }
+?>
   <a href=""><img src="../images/filter.svg" alt="filter" width="25"></a>
 </section>
 
