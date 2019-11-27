1bc900f770af3a74a5f6ff4c8181ea6c076b5f6c
samdumoulin <sam.dumoulin@student.howest.be>
2019-11-27 15:45:56 +0100

    CSS toegevoegd aan de flancers uit de database #5

--- src/view/flancers/dashboard.php
+++ src/view/flancers/dashboard.php
@@ -6,16 +6,17 @@
 
   <a href=""><img src="../images/filter.svg" alt="filter" width="25"></a>
 </section>
+<section class="flancers__overview">
   <?php
   foreach($flancers as $flancer){
     ?>
   <div class="flancer__card">
-    <div><img src="" alt=""></div>
+    <div class="img__div"><img class="flancer__image" src="../images/<?php echo $flancer["image"] ?>" width="104" alt="<?php echo $flancer["firstname"] ?>"></div>
     <div class="flancer__data">
       <p class="flancer__type"><?php echo $flancer["flancetype"] ?></p>
       <h2 class="flancer__name"><?php echo $flancer["firstname"] . ' ' . $flancer["lastname"] ?></h3>
       <p class="flancer__bio" ><?php echo $flancer["bio"] ?></p>
-        <div>
+        <div class="flancer__info">
           <p class="flancer__location"><?php echo $flancer["location"] ?></p>
           <p class="flancer__recommendations"><?php echo $flancer["recommendations"] ?> recommendations</p>
         </div>
@@ -25,5 +26,7 @@
     <?php
   }
 ?>
+</section>
+
 
 
