1dd0ed2242041c91e0fb1fe9d78d179048dda734
samdumoulin <sam.dumoulin@student.howest.be>
2019-11-27 15:58:49 +0100

    reverted changes in dashboard #5

--- src/view/flancers/dashboard.php
+++ src/view/flancers/dashboard.php
@@ -4,27 +4,23 @@
 <section class="title--section">
   <h1>Available freelancers</h1>
 
-  <a href="index.php?page=job"><img src="../images/filter.svg" alt="filter" width="25"></a>
+  <a href=""><img src="../images/filter.svg" alt="filter" width="25"></a>
 </section>
 <section class="flancers__overview">
   <?php
   foreach($flancers as $flancer){
     ?>
-  <a href ="index.php?page=detail&id=<?php echo $flancer['id']; ?>">
   <div class="flancer__card">
-    <div><img src="" alt=""></div>
-      <div class="flancer__data">
-        <p class="flancer__type"><?php echo $flancer["flancetype"] ?></p>
-        <h2 class="flancer__name"><?php echo $flancer["firstname"] . ' ' . $flancer["lastname"] ?></h3>
-        <p class="flancer__bio" ><?php echo $flancer["bio"] ?></p>
-      <div>
+    <div class="img__div"><img class="flancer__image" src="../images/<?php echo $flancer["image"] ?>" width="104" alt="<?php echo $flancer["firstname"] ?>"></div>
+    <div class="flancer__data">
+      <p class="flancer__type"><?php echo $flancer["flancetype"] ?></p>
+      <h2 class="flancer__name"><?php echo $flancer["firstname"] . ' ' . $flancer["lastname"] ?></h3>
+      <p class="flancer__bio" ><?php echo $flancer["bio"] ?></p>
+        <div class="flancer__info">
           <p class="flancer__location"><?php echo $flancer["location"] ?></p>
           <p class="flancer__recommendations"><?php echo $flancer["recommendations"] ?> recommendations</p>
-      </div>
-  </div>
-  </a>
-
-
+        </div>
+    </div>
   </div>
     <?php
   }
