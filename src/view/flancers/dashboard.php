85dc245119e0da5291e1dcaf2b839a4736c2f0f1
DewulfTjorven <tjorven.dewulf@student.howest.be>
2019-11-27 16:09:47 +0100

    routed details with id #5

--- src/view/flancers/dashboard.php
+++ src/view/flancers/dashboard.php
@@ -12,11 +12,14 @@
     ?>
   <a href ="index.php?page=detail&id=<?php echo $flancer['id']; ?>">
   <div class="flancer__card">
-    <div><img src="" alt=""></div>
+      <div>
+        <img src="" alt="">
+      </div>
       <div class="flancer__data">
         <p class="flancer__type"><?php echo $flancer["flancetype"] ?></p>
         <h2 class="flancer__name"><?php echo $flancer["firstname"] . ' ' . $flancer["lastname"] ?></h3>
         <p class="flancer__bio" ><?php echo $flancer["bio"] ?></p>
+      </div>
       <div>
           <p class="flancer__location"><?php echo $flancer["location"] ?></p>
           <p class="flancer__recommendations"><?php echo $flancer["recommendations"] ?> recommendations</p>
