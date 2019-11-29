1e1a7c291b69a68f3f54294b6eed68e0497ea980
DewulfTjorven <tjorven.dewulf@student.howest.be>
2019-11-29 14:48:35 +0100

    cleared errors on job

--- src/view/flancers/job.php
+++ src/view/flancers/job.php
@@ -1,20 +1,30 @@
 <section class="header">
 <a href="index.php?page=dashboard"><img src="../images/logo.svg" alt="Flancer logo" width="150"></a>
 </section>
+
+
+
 <section class="title__section">
   <div class="title">
     <h1 class="padding--bottom">Your jobs</h1>
-    <p class="padding--bottom"><?php
+    <p class="padding--bottom">
+      <?php
+
     if (empty($data)) {
       echo 'You did not create a job yet';
-  } else {
-  }
-    ?></p>
+    } 
+    else {
+      var_dump($data);
+    }
 
-    <section>
-    <a href="index.php?page=job" class="button-blue">Add a job</a>
-
-    </section
+      ?>
+    </p>
   </div>
+</section>
+
+
+  <section class="padding--bottom">
+  <a href="index.php?page=job" class="button-blue">Add a job</a>
+  </section>
 
 
