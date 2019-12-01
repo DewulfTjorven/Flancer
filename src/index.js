3414c543a3d76fabc16e7b6e33109bf25c4992a6
DewulfTjorven <tjorven.dewulf@student.howest.be>
2019-12-01 13:42:14 +0100

    updated form design #9

--- src/index.js
+++ src/index.js
@@ -7,6 +7,41 @@ require('./css/vars.css');
 
   const init = () => {
     
+    /*
+    function displayForm() {
+
+      var price = document.querySelector(".form__price");
+      var duration = document.querySelector(".form__duration");
+      var location = document.querySelector(".form__location");
+      var skills = document.querySelector(".form__skills");
+
+      if (price.style.display === "none") {
+        price.style.display = "block";
+      } else {
+        price.style.display = "none";
+      }
+
+      if (duration.style.display === "none") {
+        duration.style.display = "block";
+      } else {
+        duration.style.display = "none";
+      }
+
+      if (location.style.display === "none") {
+        location.style.display = "block";
+      } else {
+        location.style.display = "none";
+      }
+      
+      if (skills.style.display === "none") {
+        skills.style.display = "block";
+      } else {
+        skills.style.display = "none";
+      }
+    }
+
+    document.getElementById("showForm").addEventListener("click", displayForm);
+*/
     
   };
   init();
--- src/view/flancers/form.php
+++ src/view/flancers/form.php
@@ -10,16 +10,23 @@
 </section>
 
 <section>
-<form action="index.php?page=form" method="POST">
-    <input type="hidden" name="action" value="createjob"/>
+<form action="index.php?page=form" method="POST" class="job__form">
+    <input type="hidden" name="action" value="createjob" class=""/>
     <input type="hidden" name="page" value="added"/>
-    <input type="text" name="jobname" required />
-    <input type="text" name="description" required />
-    <input type="number" name="price" min=0 required />
-    <input type="number" name="duration" min=0 required />
-    <input type="text" name="location" required />
-    <input type="text" name="skills" required />
-    <input type="submit" class="button button--orange">Place Order</button>
+    <label for="form__jobname">Name of the job</label>
+    <input class="form__jobname padding--bottom" type="text" name="jobname" required />
+    <label for="form__textarea">Describe your job offer</label>
+    <textarea class="form__textarea" type="text" name="description" rows = "10" cols="100" required></textarea>
+    <label for="form__price" class=>Budget</label>
+    <input class="form__price" type="number" name="price" min=0 required />
+    <label for="form__duration">Duration in days</label>
+    <input class="form__duration" type="number" name="duration" min=0 required />
+    <label for="form__location">Location</label>
+    <input class="form__location" type="text" name="location" required />
+    <label for="form__skills">Required skills</label>
+    <input class="form__skills" type="text" name="skills" required />
+    <button type="submit" class="button-blue">Next step</button>
+    
   </form>
 </section>
 <?php
